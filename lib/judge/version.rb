d49cf3ce0c6d8a638e84310d15a9af60b622d75a
knut <knut.stenmark@gmail.com>
2012-05-18 16:18:16 +0200

    Initial project setup

---
+++ lib/judge/version.rb
@@ -0,0 +1,3 @@
+module Judge
+  VERSION = "0.0.1"
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'a7db275a103ebb17f47ef7769aa3bc8787a759e89e31f4560a58b11c6fc93844c0c875e168ebfa9571f6e551bffd169c878b6f72527a16cff5650b1d1bd07d58'
